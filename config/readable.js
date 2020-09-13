2029992d0e9c8fd3b1aa2e934d6b237317ed9f43
Christian Schuller <cschuller@servusalps.com>
2020-09-13 17:37:01 +0200

    Add rule configs

---
+++ config/readable.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        indent: ['error', 4],
+
+        'comma-dangle': ['error'],
+        'no-dupe-keys': ['error'],
+        'no-loss-of-precision': ['error'],
+        'no-undefined': ['error'],
+        quotes: ['error', 'double']
+    }
+};
---
+++ config/recommended.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+        'comma-dangle': ['error'],
+        'no-dupe-keys': ['error'],
+        'no-loss-of-precision': ['error'],
+        'no-undefined': ['error'],
+        quotes: ['error', 'double']
+    }
+};

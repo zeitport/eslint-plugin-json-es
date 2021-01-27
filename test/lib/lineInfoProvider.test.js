0fb57d74cd69b26e62acad9f8e9d51cddc5c73c8
Christian Schuller <cschuller@servusalps.com>
2021-01-27 16:40:12 +0100

    Improve performance (closes #12)

--- test/lib/lineInfoProvider.test.js
+++ test/lib/lineInfoProvider.test.js
@@ -1,12 +1,12 @@
-const {parseJson} = require('../../lib/parseJson');
-const {getLineInfo} = require('../../lib/getLineInfo');
+const {LineInfoProvider} = require('../../lib/lineInfoProvider');
 
 test('getLineInfo() returns line 1, column 2', () => {
     // Given
     const code = 'line1\nline2\n'
+    const lineInfoProvider = new LineInfoProvider(code);
 
     // When
-    const position = getLineInfo(code, 2);
+    const position = lineInfoProvider.getLineInfo(2);
 
     // Then
     expect(position.line).toEqual(1);
@@ -16,9 +16,10 @@ test('getLineInfo() returns line 1, column 2', () => {
 test('getLineInfo() returns line 2, column 0', () => {
     // Given
     const code = 'line1\nline2\n'
+    const lineInfoProvider = new LineInfoProvider(code);
 
     // When
-    const position = getLineInfo(code, 6);
+    const position = lineInfoProvider.getLineInfo(6);
 
     // Then
     expect(position.line).toEqual(2);
@@ -28,33 +29,12 @@ test('getLineInfo() returns line 2, column 0', () => {
 test('getLineInfo() returns line 3, column 0', () => {
     // Given
     const code = 'line1\r\nline2\r\nline3'
+    const lineInfoProvider = new LineInfoProvider(code);
 
     // When
-    const position = getLineInfo(code, 14);
+    const position = lineInfoProvider.getLineInfo(14);
 
     // Then
     expect(position.line).toEqual(3);
     expect(position.column).toEqual(0);
 });
-
-/**
- * @returns {string}
- */
-function createJson() {
-    const data = {
-        planets: [
-            {
-                name: 'Mars',
-                color: 'red'
-            },
-            {
-                name: 'Earth',
-                moons: 1,
-                dinos: [],
-                hasOxygen: true,
-            }
-        ]
-    };
-
-    return JSON.stringify(data, null, 4);
-}
